8221746c2c709145a6f95e8f34c23e113b428428
Bruno Valente <valentebruno@me.com>
2024-08-16 13:29:05 +0100

    Initial Commit

---
+++ AirApp/AirAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirAppApp.swift
+//  AirApp
+//
+//  Created by mini on 16/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct AirAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
